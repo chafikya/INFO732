9f6f1179bfb59bf0c4c8f71a41519f532be28cfa
coltarm <113551889+coltarm@users.noreply.github.com>
2023-11-07 15:03:27 +0100

    ajout classe anne factory et modif anne, semaine et mois

--- Proj_concept/src/Mois.java
+++ Proj_concept/src/Mois.java
@@ -2,5 +2,13 @@ import java.util.ArrayList;
 
 public class Mois {
 	ArrayList<Semaine> sem = new ArrayList<Semaine>();
+	
+	public Mois(Annee an) {
+		an.addMois(this);
+	}
+	
+	public void addSem(Semaine semaine) {
+		sem.add(semaine);
+	}
 
 }
--- Proj_concept/src/Semaine.java
+++ Proj_concept/src/Semaine.java
@@ -2,6 +2,12 @@ import java.util.ArrayList;
 
 public class Semaine {
 	ArrayList<Cours> edt = new ArrayList<Cours>();
+	int number;
+	public Semaine(int num, Mois mois) {
+		number = num;
+		mois.addSem(this);
+		
+	}
 	
 
 }
